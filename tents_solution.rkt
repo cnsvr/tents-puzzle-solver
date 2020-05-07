6738c2e89956465efd8ba09aef78bd059a3d4b85
Furkan Cansever <furkan.cansever@boun.edu.tr>
2020-05-08 01:15:06 +0300

    Helper functions are done.

--- tents_solution.rkt
+++ tents_solution.rkt
@@ -1,2 +1,110 @@
 #lang scheme
 ;2016400348
+
+; HELPER FUNCTIONS
+
+;This function takes 2 parameters. 
+;First parameter will be a function and the second parameter will be a list. 
+;This function would apply the given function to each element of the list in order. 
+;If the given function returns something that is not false, 
+;then this function should return the return value as well. 
+;If the given func- tion returns false for each and every element of the list 
+;then this function should return false as well.
+
+(define (RETURN-FIRST-NOT-FALSE predicate list )
+  (if (null? list)
+  #f
+  (if (eq? (predicate (car list)) #f )
+    (RETURN-FIRST-NOT-FALSE predicate (cdr list))
+    (predicate (car list))
+  )))
+
+;This function takes 2 parameters. 
+;Both parameters will be lists with 2 integers representing a position on a grid. 
+;This function should return true if these positions are adjacent, including diago- nals, or the same position. 
+;Otherwise it should return false.
+
+(define (ADJACENT p1 p2)
+  (if (<= (+ (abs (- (car p1) (car p2))) (abs (- (cadr p1) (cadr p2)))) 2)
+    #t
+    ;else
+    #f  
+  )
+)
+
+;This function takes a single parameter. 
+;It will be a list with 2 integers representing a position on a grid. 
+;This function should return a list of neighboring positions, horizontally and vertically. 
+;The function can return the neighbors in any order. 
+;Even if a position is out of bounds in the context of the tents puzzle this function must put that position in the list that this function returns.
+
+(define (NEIGHBOR-LIST list)
+  (cons   
+  ; LEFT
+  (cons (- (car list) 1) (cons (cadr list) '()))     
+  (cons
+  ; RIGHT 
+  (cons (+ (car list) 1) (cons (cadr list) '()))  
+  (cons
+  ; UP
+  (cons (car list) (cons (- (cadr list) 1) '()))   
+  (cons
+  ; DOWN
+  (cons (car list) (cons (+ (cadr list) 1) '())) '()))))
+)
+
+;This function takes 2 parameters. 
+;A list containing 2 integers and a list of lists that are containing 2 integers.
+;The first parameter represents a position on a grid and each element on the second list also represents a position. 
+;This function should return true if the position represented 
+;by the first parameter is adjacent to at least one of the positions in the given list (using the same definition for adjacency from the ADJACENT function). 
+;Otherwise it should return false
+
+
+(define (ADJACENT-WITH-LIST list listOfList)
+  (if (null? listOfList)
+  #f
+  (if (ADJACENT list (car listOfList))
+  #t
+  (ADJACENT-WITH-LIST list (cdr listOfList))
+  ))
+)
+
+;This function takes 3 parameters. 
+;A list, an integer indicating a position on this list and a value. 
+;It should return a list that is identical to the given list, except the value in the given position should be replaced with the given value. 
+;The given position will always be positive and not bigger than the length of the given list.
+
+(define (REPLACE-NTH list pos val)
+  (if (eq? pos 1)
+  (cons val (cdr list))
+  (cons (car list) (REPLACE-NTH (cdr list) (- pos 1) val ))
+  )
+)
+
+;This function takes 2 parameters.
+;A point of grid and size of puzzle (NxN)
+;It returns true if given point is in bound of puzzle, otherwise false.
+
+(define (IS-IN-BOUND point SIZE ) 
+  (if (and (and (>= (car point) 1) (<= (car point) SIZE)) (and (>= (cadr point) 1) (<= (cadr point) SIZE)))
+  #t
+  #f
+  )
+)
+
+; TENTS SOLVER
+; row -> list cols -> list  trees -> list of list
+(define (TENTS-SOLUTION row cols trees)
+  
+
+
+
+
+)
+
+
+
+
+
+
